8a99bc88639a182c0d88fceb09629d8a95c0f9cf
string-12 <ethan920912@gmail.com>
2024-10-28 15:35:01 +0800

    all spin0 couple

--- run5.sh
+++ run5.sh
@@ -1,11 +1,14 @@
 list=(25.0 33.0 50.0)
-spinlist=(0 1 0.1 0.25 0.5 2)
+spinlist=(0.0 1.0 0.1 0.25 0.5 2.0)
+dir=$(pwd)
+echo "$dir"
 #rm -rf inter_result.txt
-for ((j = 0; j < ${#spinlist[@]}; j++))
+for ((j = 1; j < ${#spinlist[@]}; j++))
 do
 for ((i = 0; i < ${#list[@]}; i++)) 
     do
         echo "Start process"
+	rm -r out.ck
         #rm -rf  sdp.zip.ck  sdp.zip_out  sdp.zip  division.txt result.txt
 
         mu=${list[i]}
@@ -17,14 +20,14 @@ for ((i = 0; i < ${#list[@]}; i++))
         wolframscript -file extremal_range_spin0.m
 
         echo "Turning xml to sdp file"
-        docker run --shm-size=4096m -v /home/string-12/sdpb:/data bootstrapcollaboration/sdpb:master pmp2sdp 1024 -i /data/out.json -o /data/out
+        docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master pmp2sdp 1024 -i /data/out.json -o /data/out
 
 	echo "Running sdpb"
-        docker run --shm-size=4096m -v /home/string-12/sdpb:/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 32 sdpb --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
+        docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 32 sdpb --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
 	
 	touch spin0/spin0_result_"$j".txt
         
-	python3 store_result.py out/out.txt spin0/spin0_result_"$j".txt
+	python3 store_result.py out_out/out.txt spin0/spin0_result_"$j".txt
 
         #rm -rf upper_spectrum 
 
