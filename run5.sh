f81a6c298f09b92d11f5cc361f9ccea68826b6e9
string-12 <ethan920912@gmail.com>
2024-10-29 01:46:29 +0800

    save for all result

--- run5.sh
+++ run5.sh
@@ -1,9 +1,9 @@
-list=(25.0 33.0 50.0)
-spinlist=(0.0 1.0 0.1 0.25 0.5 2.0)
+list=(1.0 1.3 2.0 3.0 5.0 10.0)
+spinlist=(1.0 0.5)
 dir=$(pwd)
 echo "$dir"
 #rm -rf inter_result.txt
-for ((j = 1; j < ${#spinlist[@]}; j++))
+for ((j = 0; j < ${#spinlist[@]}; j++))
 do
 for ((i = 0; i < ${#list[@]}; i++)) 
     do
@@ -23,11 +23,11 @@ for ((i = 0; i < ${#list[@]}; i++))
         docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master pmp2sdp 1024 -i /data/out.json -o /data/out
 
 	echo "Running sdpb"
-        docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 32 sdpb --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
+        docker run --shm-size=4096m -v "$dir":/data bootstrapcollaboration/sdpb:master mpirun --allow-run-as-root -n 48 sdpb --precision=1024 --procsPerNode=32 --maxIterations=5000 -s /data/out
 	
 	touch spin0/spin0_result_"$j".txt
         
-	python3 store_result.py out_out/out.txt spin0/spin0_result_"$j".txt
+	python3 store_result.py out_out/out.txt spin0/spin0_result_0_"$j".txt
 
         #rm -rf upper_spectrum 
 
